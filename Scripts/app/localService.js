72bbad9a6be738c9d979c492f492492c313d80d0
ryan_ <ryan_@LAPTOP-MN0ALGFJ>
2016-12-19 10:53:16 -0800

    ls path

--- Scripts/app/localService.js
+++ Scripts/app/localService.js
@@ -2,7 +2,7 @@
 
 executivesServices.factory('Executives', ['$resource',
   function ($resource) {
-      return $resource('/Scripts/app/executive.json', {}, {
+      return $resource('Scripts/app/executive.json', {}, {
           query: { method: 'GET', params: {}, isArray: true }
       });
   }]);
